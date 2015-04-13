ea89c8de6d2db70a173d63654a565a22bc0d40a9
JussaraGomes <jussarauefs@gmail.com>
2015-04-13 12:51:15 -0300

    Atualização da ULA

--- rtl/ULA.sv
+++ rtl/ULA.sv
@@ -1,63 +1,105 @@
-/* 
-Módulo da ULA, Unidade Logica e Aritmetica
-@author Jussara Machado
-@author Jhone Almeida
-*/		
-		
-module ULA(operandoA, operandoB, resultadoOp, controle);
+module ULA(operandoA, operandoB, resultadoOp, controle, Z, C, S, O);
   
     parameter bits_palavra = 16;
     parameter bits_controle = 5;
     
-		input [bits_controle-1:0] controle; // [16] -> bit de sinal e [15:0] -> valor
-		input signed [bits_palavra-1:0] operandoA, operandoB;
-		output reg signed [bits_palavra-1:0] resultadoOp;
-		output reg //Z,  Zero - (Este bit fica a 1 quando o resultado da operação for 0)
+		input signed [bits_controle-1:0] controle; // [00000 - 11111]
+		input signed [bits_palavra-1:0] operandoA, operandoB; // [15] -> bit de sinal e [14:0] -> valor (de -32768 à 32767)
+		output reg signed [bits_palavra-1:0] resultadoOp; // [15] -> bit de sinal e [14:0] -> valor (de -32768 à 32767)
+		
+		reg signed [bits_palavra:0] aux_resultadoOp; // [16]-> bit de sinal e [15]-> overflow [14:0]-> valor
+		
+
+
+		output reg Z, // Zero - (Este bit fica a 1 quando o resultado da operação for 0)
 		           C, // Carry - (Indicar que há um bit de transporte) 
 		              // Em qualquer das formas de deslocamento o bit de estado transporte recebe o bit que se perde com o deslocamento, o bit mais
                   // significativo do operando no caso de deslocamentos à esquerda, ou o bit menos significativo nos deslocamentos à direita.
-		           //N, // Sinal - (O bit mais significativo do resultado. Este bit indica quando o resultado deu negativo)
+		           S, // Sinal - (O bit mais significativo do resultado. Este bit indica quando o resultado deu negativo)
 		           O; // overflow (Quando o resultado tem uma magnitude que excede o valor máximo possível de representar com o número de bits disponíveis para o resultado)
 		
 				
-		always @(operandoA or operandoB or controle)
+		always @(operandoA or operandoB or controle) 
 		begin 
 		  
 			case(controle)
 			5'b00000: begin // 00000 C = A + B
-			   resultadoOp = operandoA + operandoB;
+			   aux_resultadoOp = operandoA + operandoB;
+			   			   
+			   O = aux_resultadoOp[15];
+			   
+			   resultadoOp = { aux_resultadoOp[16] , aux_resultadoOp [14:0]};
+			   
+			   
+			   if(operandoA[15] == operandoA[15])
+			     if(resultadoOp[15] != operandoA[15])
+			       C = 1'b0;  
+			       
 				end
 				
 			5'b00001: begin // 00001 C = A + B + 1
-			   resultadoOp = operandoA + operandoB + 1;
+			   aux_resultadoOp = operandoA + operandoB + 1;			
+			   
+			   O = aux_resultadoOp[15];
+			   
+			   resultadoOp = { aux_resultadoOp[16] , aux_resultadoOp [14:0]};
+			   
+			   
+			   if(operandoA[15] == operandoA[15])
+			     if(resultadoOp[15] != operandoA[15])
+			       C = 1'b0;  
 				end
 				
 			5'b00011: begin // 00011 C = A + 1
-			   resultadoOp = operandoA + 1; 
+			   aux_resultadoOp = operandoA + 1;
+			   
+			   O = aux_resultadoOp[15];
+			   
+			   resultadoOp = { aux_resultadoOp[16] , aux_resultadoOp [14:0]};
+			   
+			   
+			   if(operandoA[15] == operandoA[15])
+			     if(resultadoOp[15] != operandoA[15])
+			       C = 1'b0;  
 				end
 				
-			5'b00100: begin // 00100 C = A - B - 1
-			   resultadoOp = operandoA - operandoB - 1;
+			5'b00100: begin // 00100 C = A - B - 1 **********************************************
+			   aux_resultadoOp = operandoA - operandoB - 1;
+			   
+			   if((aux_resultadoOp < -32768) || (aux_resultadoOp > 32767))
+			     O = 1'b0;
+			   
+			   resultadoOp = { aux_resultadoOp[16] , aux_resultadoOp [14:0]};
 				end
 				
-			5'b00101: begin // 00101 C = A - B
-			   resultadoOp = operandoA - operandoB;
+			5'b00101: begin // 00101 C = A - B ********************************************
+			   aux_resultadoOp = operandoA - operandoB;
+			   
+			   if((aux_resultadoOp < -32768) || (aux_resultadoOp > 32767))
+			     O = 1'b0;
+			   
+			   resultadoOp = { aux_resultadoOp[16] , aux_resultadoOp [14:0]};
 				end
 				
-			5'b00110: begin // 00110 C = A - 1
-			   resultadoOp = operandoA - 1;
+			5'b00110: begin // 00110 C = A - 1  ****************************************
+			   aux_resultadoOp = operandoA - 1;
+			   
+			   if((aux_resultadoOp < -32768) || (aux_resultadoOp > 32767))
+			     O = 1'b0;
+			   
+			   resultadoOp = { aux_resultadoOp[16] , aux_resultadoOp [14:0]};
 				end
 				
 			5'b01000: begin // 01000 C = Deslocamento Lógico Esq. (A)
-			   //C = operandoA[16]; // Assume o bit mais significativo (perdido depois do deslocamento)
+			   C = operandoA[16]; // Assume o bit mais significativo (perdido depois do deslocamento)
 			   resultadoOp = operandoA << 1;
 				end
 				
 			5'b01001: begin // 01001 C = Deslocamento Aritmético Dir. (A)
-			   //C = operandoA[0]; // Assume o bit menos significativo (perdido depois do deslocamento)
+			   C = operandoA[0]; // Assume o bit menos significativo (perdido depois do deslocamento)
 			   resultadoOp = operandoA >>> 1;
 				end
-				
+							
 			5'b10000: begin // 10000 C = 0 
 			   resultadoOp = 0;
 				end
@@ -123,21 +165,17 @@ module ULA(operandoA, operandoB, resultadoOp, controle);
 				end
 				
 			default: begin
-				resultadoOp = controle;
+			   resultadoOp = 0;
 				end
 			endcase // fim do case
 			
 			
-			// *** Eu quero deslocar um número com sinal, há diferença entre deslocar um número com sinal e deslocar um número sem sinal?
-			// *** A lógica para o cálculo do bit de estado carry (C) é calculada fazendo-se uma and entre os dois bits mais significativos dos operandos?		
-			C = 0;
-			for (i = 0; i < 16; i = i +1) begin
-				C = (C & (operandoA[i] | operandoB[i])) | (operandoA[i] & operandoB[i]);
-			end
-			//N = resultadoOp[16]; // Sinal -  Atualiza o bit de sinal, se o bit for 0, o resultado da operação foi positiva, se o bit for 1 o resultado foi negativo
+			// *** Eu quero deslocar um número com sinal, há diferença entre deslocar um número com sinal e deslocar um número sem sinal?	
 			
-			//if(resultadoOp == 0) // Zero - Verifica se o resultado da operação foi 0
-			 // Z = 1'b0;
+			S = resultadoOp[15]; // Sinal -  Atualiza o bit de sinal, se o bit for 0, o resultado da operação foi positiva, se o bit for 1 o resultado foi negativo
+			
+			if(resultadoOp == 0) // Zero - Verifica se o resultado da operação foi 0
+			  Z = 1'b0;
 			
 			
 		end
