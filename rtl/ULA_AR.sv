eed1fd0f3c86b883b845e097cedc6e90f228c260
fabugo <fabugo@gmail.com>
2015-08-07 13:56:54 -0300

    Revert "Tentativa de remover bug"
    This reverts commit 30a0a18140feeca8fc024349ed6121d32f00632a.

--- rtl/ULA_AR.sv
+++ rtl/ULA_AR.sv
@@ -16,13 +16,13 @@ module ULA_AR (
 	parameter bits=3;
 
 	input reg signed [bits-1:0] A,B;	//dados para operacao
-	input reg [4:0] OP;					//tipo de operacao
+	input reg [4:0] OP;			//tipo de operacao
 	output reg signed [bits-1:0] RESU;	//resultado da operacao
-	output logic 	O,					//flag que indica se ouve overflow na operacao
-					C,					//flag que indica se ouve carryout na operacao
-					S,					//flag que indica o sinal do resultado da operacao
-					Z;					//flag que indica que o resultado da operacao é Zero
-	logic signed [bits:0] AUX;			//Auxilia na identificação de carry e overflow
+	output logic 	O,			//flag que indica se ouve overflow na operacao
+					C,			//flag que indica se ouve carryout na operacao
+					S,			//flag que indica o sinal do resultado da operacao
+					Z;			//flag que indica que o resultado da operacao é Zero
+	logic [bits:0] AUX;			//Auxilia na identificação de carry e overflow
 
 	always @(A or B or OP) begin
 		case (OP)
@@ -32,7 +32,7 @@ module ULA_AR (
 			5'b00100: AUX = A-B-1;
 			5'b00101: AUX = A-B;
 			5'b00110: AUX = A-1;
-			default:;
+			default : AUX = OP;
 		endcase
 		case (OP[2:1])
 			2'b00:if((A[bits-1] == B[bits-1]) && (RESU[bits-1] != A[bits-1]))
@@ -51,18 +51,12 @@ module ULA_AR (
 					O = 1;
 				else
 					O = 0;
-			default:;
+			default :AUX = OP;
 		endcase
-		
-		if (A==~B)
-			C = 1;
-		else
-			C = AUX[bits];
+		C = AUX[bits];
 		RESU = AUX;
 		S = RESU [bits-1];
 		if(!RESU)
 			Z = 1;
-		else
-			Z = 0;
 	end
 endmodule
--- sim/tb/ULA_AR_TB.sv
+++ sim/tb/ULA_AR_TB.sv
@@ -1,30 +1,21 @@
-include "rtl\\ULA_AR.sv";
+include "ULA_AR.sv";
 
 module ULA_AR_TB;
-	parameter bits=3;
 
-	reg signed [bits-1:0] A,B;
+	reg [2:0] A,B;
 	reg [4:0] OP;
-	reg signed [bits-1:0] RESU;
+	reg [2:0] RESU;
 	logic 	O,
 			C,
 			S,
 			Z;
-	
-	reg signed [bits-1:0] a2;
-
-	logic signed [bits:0] AUXI;
 
 	ULA_AR u(A,B,OP,RESU,O,C,S,Z);
 
 	initial begin
 		OP = 5'b00000;
-		A = 3'b010;
-		B = 3'b110;
+		A = 1;
+		B = -1;
 		#1;
-		a2 = (~B)+1;
-		if(A == a2)
-			$display ("Hello World");
-		AUXI = A+B;
 	end
 endmodule
