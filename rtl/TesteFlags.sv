00a25a734650bcc583fb05fbf474c45edc4f5fcf
JussaraGomes <jussarauefs@gmail.com>
2015-09-24 13:39:50 -0300

    Teste

--- rtl/TesteFlags.sv
+++ rtl/TesteFlags.sv
@@ -1,7 +1,7 @@
 include "ULA.sv";
 
 module TesteFlags();
-    parameter n = 30;
+    parameter n = 57;
 	reg [7:0] OP;			
 	reg signed [bits-1:0] A,B,RESU, RESU_ESP;	
 	reg	O,C,S,Z, EO,EC,ES,EZ;					
@@ -12,7 +12,7 @@ module TesteFlags();
 	initial begin
 		file = $fopen("TesteFlags.out");
 		
-		for(int i=0; i<n; i++) begin
+		for(int i=1; i<n; i++) begin
 			case(i)
 			// --------------------------------------------------------------------------
 			// Levando em consideração complemento a2
@@ -25,7 +25,7 @@ module TesteFlags();
 				B = 16'b0010000000000000;				        
 			end
 			
-			1: begin
+			2: begin
 				// Caso II: Um número positivo e outro menor e negativo.
 				$fdisplay(file,"OP = 00000 	C = A + B  								todas");		
 				OP = 10001; EO = 0; EC = 1; ES = 0; EZ = 0;
@@ -33,7 +33,7 @@ module TesteFlags();
 				B = 16'b1110000000000000;				        
 			end       
 			
-			1: begin
+			3: begin
 				// Caso III: Um número positivo e outro maior e negativo.
 				$fdisplay(file,"OP = 00000 	C = A + B  								todas");		
 				OP = 10001; EO = 0; EC = 0; ES = 1; EZ = 0;
@@ -41,7 +41,7 @@ module TesteFlags();
 				B = 16'b0010000000000000;	
 			end   
 			
-			1: begin
+			4: begin
 				// Caso IV: Dois números negativos.
 				$fdisplay(file,"OP = 00000 	C = A + B  								todas");		
 				OP = 10001; EO = 0; EC = 1; ES = 1; EZ = 0;
@@ -49,54 +49,67 @@ module TesteFlags();
 				B = 16'b1110000000000000;	
 			end 
 			
-			1: begin
-				// Caso IV: Dois números iguais em magnetude mas de sinais contrarios.
+			5: begin
+				// Caso V: Dois números iguais em magnetude mas de sinais contrarios.
 				$fdisplay(file,"OP = 00000 	C = A + B  								todas");		
 				OP = 10001; EO = 0; EC = 1; ES = 0; EZ = 1;
 				A = 16'b1011100000000000;
 				B = 16'b0100100000000000;	
 			end 
 			
-			1: begin	$fdisplay(file,"OP = 00001 	C = A + B + 1   						todas");
-			1: begin	$fdisplay(file,"OP = 00011 	C = A + 1  								todas");
-			1: begin	$fdisplay(file,"OP = 00100 	C = A – B – 1   						todas");
-			1: begin	$fdisplay(file,"OP = 00101 	C = A – B   							todas");
-			1: begin	$fdisplay(file,"OP = 00110 	C = A – 1   							todas");
-				
-			1: begin	$fdisplay(file,"OP = 01000 	C = Deslocamento Lógico Esq. (A)   		S, C, Z");
-				
-				// --------------------------------------------------------------------------
-			1: begin	$fdisplay(file,"OP = 01001 	C = Deslocamento Aritmético Dir. (A)  	S, C, Z");
-				
-				OP = 10100; EO = 0; EC = 0; ES = 1; EZ = 0;
-				A = 16'b1010111001101111;
-				
-				
-				
-				
-				
-				
 			// --------------------------------------------------------------------------
-			1: begin	
+			6: begin
+				// Caso I: Dois números positivos.
+				$fdisplay(file,"OP = 00001 	C = A + B + 1   						todas");
+				OP = 00001; EO = 0; EC = 0; ES = 0; EZ = 0;
+				A = 16'b0100100000000000;
+				B = 16'b0010000000000000;
+			end
+			
+			7: begin
+				// Caso II: Um número positivo e outro menor e negativo.
+				$fdisplay(file,"OP = 00001 	C = A + B + 1   						todas");		
+				OP = 00001; EO = 0; EC = 1; ES = 0; EZ = 0;
+				A = 16'b0100100000000000;
+				B = 16'b1110000000000000;				        
+			end 
+			
+			8: begin
+				// Caso III: Um número positivo e outro maior e negativo.
+				$fdisplay(file,"OP = 00001 	C = A + B + 1   						todas");	
+				OP = 00001; EO = 0; EC = 0; ES = 1; EZ = 0;
+				A = 16'b1011100000000000;
+				B = 16'b0010000000000000;	
+			end   
+						
+			// --------------------------------------------------------------------------
+			9: begin	$fdisplay(file,"OP = 00011 	C = A + 1  								todas"); end
+			10: begin	$fdisplay(file,"OP = 00100 	C = A – B – 1   						todas"); end
+			11: begin	$fdisplay(file,"OP = 00101 	C = A – B   							todas"); end
+			12: begin	$fdisplay(file,"OP = 00110 	C = A – 1   							todas"); end
+			13: begin	$fdisplay(file,"OP = 01000 	C = Deslocamento Lógico Esq. (A)   		S, C, Z"); end
+			14: begin	$fdisplay(file,"OP = 01001 	C = Deslocamento Aritmético Dir. (A)  	S, C, Z"); end
+			// --------------------------------------------------------------------------
+			15: begin	
 				$fdisplay(file,"OP = 10000 	C = 0    								Z");			
 				OP = 10000; EO = 0; EC = 0; ES = 0; EZ = 1;
 			end	
 			// --------------------------------------------------------------------------
-			1: begin	
+			16: begin	
 				$fdisplay(file,"OP = 10001 	C = A&B                                 Z, S");			
 				OP = 10001; EO = 0; EC = 0; ES = 1; EZ = 0;
 				A = 16'b1010101010111010;
 				B = 16'b1101001101010111;
 			end
 			
-			1: begin	
+			17: begin	
 				$fdisplay(file,"OP = 10001 	C = A&B                                 Z, S");	
 				OP = 10001; EO = 0; EC = 0; ES = 0; EZ = 0;
 				A = 16'b0010101010101000;
 				B = 16'b1101010101010111;
 			end
 			
-			1: begin	
+			18: begin	
 				$fdisplay(file,"OP = 10001 	C = A&B                                 Z, S");	
 				OP = 10001; EO = 0; EC = 0; ES = 0; EZ = 1;
 				A = 16'b0101010101010101;
@@ -104,21 +117,21 @@ module TesteFlags();
 			end
 			
 			// --------------------------------------------------------------------------
-			1: begin	
+			19: begin	
 				$fdisplay(file,"OP = 10010 	C = !A&B 								Z, S");		
 				OP = 10100; EO = 0; EC = 0; ES = 0; EZ = 0;
 				A = 16'b1010111001101111;
 				B = 16'b0111001101110110;
 			end
 			
-			1: begin	
+			20: begin	
 				$fdisplay(file,"OP = 10010 	C = !A&B 								Z, S");
 				OP = 10100; EO = 0; EC = 0; ES = 0; EZ = 1;
 				A = 16'b0101101011011101;
 				B = 16'b1101101111011101;
 			end
 			
-			1: begin	
+			21: begin	
 				$fdisplay(file,"OP = 10010 	C = !A&B 								Z, S");
 				OP = 10100; EO = 0; EC = 0; ES = 0; EZ = 0;
 				A = 16'b1111110111011101;
@@ -126,40 +139,40 @@ module TesteFlags();
 			end
 			// --------------------------------------------------------------------------
 						
-			1: begin	
+			22: begin	
 				$fdisplay(file,"OP = 10011 	C = B  									Z, S");			
 				OP = 10101; EO = 0; EC = 0; ES = 0; EZ = 0;
 				B = 16'b0101101011011101;
 			end
 			
-			1: begin	
+			23: begin	
 				$fdisplay(file,"OP = 10011 	C = B  									Z, S");	
 				OP = 10101; EO = 0; EC = 0; ES = 0; EZ = 1;
 				B = 16'b0000000000000000;
 			end
 			
-			1: begin
+			24: begin
 				$fdisplay(file,"OP = 10011 	C = B  									Z, S");			
 				OP = 10101; EO = 0; EC = 0; ES = 1; EZ = 0;
 				B = 16'b1111101011011101;
 			end
 			
 			// --------------------------------------------------------------------------
-			1: begin	
+			25: begin	
 				$fdisplay(file,"OP = 10100 	C = A&!B  								Z, S");			
 				OP = 10100; EO = 0; EC = 0; ES = 1; EZ = 0;
 				A = 16'b1010111001101111;
 				B = 16'b0111001101110110;
 			end
 			
-			1: begin	
+			26: begin	
 				$fdisplay(file,"OP = 10100 	C = A&!B  								Z, S");			
 				OP = 10100; EO = 0; EC = 0; ES = 0; EZ = 0;
 				A = 16'b0010111001101111;
 				B = 16'b0111001101110110;
 			end
 			
-			1: begin	
+			27: begin	
 				$fdisplay(file,"OP = 10100 	C = A&!B  								Z, S");	
 				OP = 10100; EO = 0; EC = 0; ES = 0; EZ = 1;
 				A = 16'b0101101011011101;
@@ -167,40 +180,40 @@ module TesteFlags();
 			end
 			
 			// --------------------------------------------------------------------------
-			1: begin	
+			28: begin	
 				$fdisplay(file,"OP = 10101 	C = A  									Z, S");			
 				OP = 10101; EO = 0; EC = 0; ES = 0; EZ = 0;
 				A = 16'b0101101011011101;
 			end
 			
-			1: begin	
+			29: begin	
 				$fdisplay(file,"OP = 10101 	C = A  									Z, S");	
 				OP = 10101; EO = 0; EC = 0; ES = 0; EZ = 1;
 				A = 16'b0000000000000000;
 			end
 			
-			1: begin
+			30: begin
 				$fdisplay(file,"OP = 10101 	C = A  									Z, S");			
 				OP = 10101; EO = 0; EC = 0; ES = 1; EZ = 0;
 				A = 16'b1111101011011101;
 			end
 			
 			// --------------------------------------------------------------------------
-			1: begin	
+			31: begin	
 				$fdisplay(file,"OP = 10110 	C = A xor B  							Z, S");				
 				OP = 10110; EO = 0; EC = 0; ES = 1; EZ = 0;
 				A = 16'b0010101010111010;
 				B = 16'b1101001101010111;
 			end
 			
-			1: begin	
+			32: begin	
 				$fdisplay(file,"OP = 10110 	C = A xor B  							Z, S");
 				OP = 10110; EO = 0; EC = 0; ES = 0; EZ = 0;
 				A = 16'b1010101010101000;
 				B = 16'b1101010101010111;
 			end
 			
-			1: begin	
+			33: begin	
 				$fdisplay(file,"OP = 10110 	C = A xor B  							Z, S");
 				OP = 10110; EO = 0; EC = 0; ES = 0; EZ = 1;
 				A = 16'b1010101010101010;
@@ -208,21 +221,21 @@ module TesteFlags();
 			end
 			
 			// --------------------------------------------------------------------------
-			1: begin	
+			34: begin	
 				$fdisplay(file,"OP = 10111 	C = A | B 								Z, S");			
 				OP = 10111; EO = 0; EC = 0; ES = 1; EZ = 0;
 				A = 16'b1010101100011011;
 				B = 16'b0010101001010011;
 			end
 			
-			1: begin	
+			35: begin	
 				$fdisplay(file,"OP = 10111 	C = A | B 								Z, S");	
 				OP = 10111; EO = 0; EC = 0; ES = 0; EZ = 1;
 				A = 16'b1010101010101010;
 				B = 16'b0101010101010101;
 			end
 			
-			1: begin	
+			36: begin	
 				$fdisplay(file,"OP = 10111 	C = A | B 								Z, S");	
 				OP = 10111; EO = 0; EC = 0; ES = 0; EZ = 0;
 				A = 16'b0001011101101111;
@@ -230,42 +243,42 @@ module TesteFlags();
 			end
 			
 			// --------------------------------------------------------------------------
-			1: begin	
+			37: begin	
 				$fdisplay(file,"OP = 11000 	C = !A&!B  								Z, S");			
 				OP = 11000; EO = 0; EC = 0; ES = 1; EZ = 0;
 				A = 16'b0010101010111010;
 				B = 16'b0101001101010111;
 			end
 			
-			1: begin	
+			38: begin	
 				$fdisplay(file,"OP = 11000 	C = !A&!B  								Z, S");	
 				OP = 11000; EO = 0; EC = 0; ES = 0; EZ = 1;
 				A = 16'b0010101010101000;
 				B = 16'b1101010101010111;
 			end
 			
-			1: begin	
+			39: begin	
 				$fdisplay(file,"OP = 11000 	C = !A&!B  								Z, S");	
 				OP = 11000; EO = 0; EC = 0; ES = 0; EZ = 0;
 				A = 16'b1010101010111111;
 				B = 16'b0101010101010111;
 			end
 			// --------------------------------------------------------------------------	
-			1: begin	
+			40: begin	
 				$fdisplay(file,"OP = 11001 	C = !(A xor B) 							Z, S"); 			
 				OP = 11001; EO = 0; EC = 0; ES = 1; EZ = 0;
 				A = 16'b1010101010111010;
 				B = 16'b1101001101010111;
 			end
 			
-			1: begin	
+			41: begin	
 				$fdisplay(file,"OP = 11001 	C = !(A xor B) 							Z, S"); 
 				OP = 11001; EO = 0; EC = 0; ES = 0; EZ = 0;
 				A = 16'b1010101010101000;
 				B = 16'b0101010101010111;
 			end
 			
-			1: begin	
+			42: begin	
 				$fdisplay(file,"OP = 11001 	C = !(A xor B) 							Z, S"); 
 				OP = 11001; EO = 0; EC = 0; ES = 0; EZ = 1;
 				A = 16'b1111111111111111;
@@ -273,40 +286,40 @@ module TesteFlags();
 			end
 			
 			// --------------------------------------------------------------------------
-			1: begin	
+			43: begin	
 				$fdisplay(file,"OP = 11010 	C = !A 									Z, S");			
 				OP = 11010; EO = 0; EC = 0; ES = 0; EZ = 0;
 				A = 16'b1010101010111111;
 			end
 			
-			1: begin	
+			44: begin	
 				$fdisplay(file,"OP = 11010 	C = !A 									Z, S");	
 				OP = 11010; EO = 0; EC = 0; ES = 1; EZ = 0;
 				A = 16'b0010101010111111;
 			end
 			
-			1: begin	
+			45: begin	
 				$fdisplay(file,"OP = 11010 	C = !A 									Z, S");	
 				OP = 11010; EO = 0; EC = 0; ES = 0; EZ = 1;
 				A = 16'b0000000000000000;
 			end
 			
 			// --------------------------------------------------------------------------
-			1: begin	
+			46: begin	
 				$fdisplay(file,"OP = 11011 	C = !A|B 								Z, S"); 				
 				OP = 11011; EO = 0; EC = 0; ES = 1; EZ = 0;
 				A = 16'b1010101010111010;
 				B = 16'b1101001101010111;
 			end
 			
-			1: begin	
+			47: begin	
 				$fdisplay(file,"OP = 11011 	C = !A|B 								Z, S"); 	
 				OP = 11011; EO = 0; EC = 0; ES = 0; EZ = 0;
 				A = 16'b1010101010101000;
 				B = 16'b0101010101010111;
 			end
 			
-			1: begin	
+			48: begin	
 				$fdisplay(file,"OP = 11011 	C = !A|B 								Z, S"); 	
 				OP = 11011; EO = 0; EC = 0; ES = 0; EZ = 1;
 				A = 16'b1111111111111111;
@@ -314,40 +327,40 @@ module TesteFlags();
 			end
 			
 			// --------------------------------------------------------------------------
-			1: begin	
+			49: begin	
 				$fdisplay(file,"OP = 11100 	C = !B 									Z, S");				
 				OP = 11100; EO = 0; EC = 0; ES = 0; EZ = 0;
 				B = 16'b1010101010111111;
 			end
 			
-			1: begin	
+			50: begin	
 				$fdisplay(file,"OP = 11100 	C = !B 									Z, S");	
 				OP = 11100; EO = 0; EC = 0; ES = 1; EZ = 0;
 				B = 16'b0010101010111111;
 			end
 			
-			1: begin	
+			51: begin	
 				$fdisplay(file,"OP = 11100 	C = !B 									Z, S");	
 				OP = 11100; EO = 0; EC = 0; ES = 0; EZ = 1;
 				B = 16'b0000000000000000;
 			end
 			
 			// --------------------------------------------------------------------------
-			1: begin	
+			52: begin	
 				$fdisplay(file,"OP = 11101 	C = A|!B 								Z, S");			
 				OP = 11101; EO = 0; EC = 0; ES = 1; EZ = 0;
 				A = 16'b1010101010111010;
 				B = 16'b0101001101010111;
 			end 
 			
-			1: begin
+			53: begin
 				$fdisplay(file,"OP = 11101 	C = A|!B 								Z, S");	
 				OP = 11101; EO = 0; EC = 0; ES = 0; EZ = 0;
 				A = 16'b0010101010101000;
 				B = 16'b1101010101010111;
 			end
 			
-			1: begin
+			54: begin
 				$fdisplay(file,"OP = 11101 	C = A|!B 								Z, S");	
 				OP = 11101; EO = 0; EC = 0; ES = 0; EZ = 1;
 				A = 16'b0000000000000000;
@@ -355,21 +368,21 @@ module TesteFlags();
 			end
 			
 			// --------------------------------------------------------------------------	
-			1: begin	
+			55: begin	
 				$fdisplay(file,"OP = 11110 	C = !A|!B 								Z, S");				
 				OP = 11110; EO = 0; EC = 0; ES = 1; EZ = 0;
 				A = 16'b1010101010111010;
 				B = 16'b0101001101010111;
 			end		
 			
-			1: begin	
+			56: begin	
 				$fdisplay(file,"OP = 11110 	C = !A|!B 								Z, S");
 				OP = 11110; EO = 0; EC = 0; ES = 0; EZ = 0;
 				A = 16'b1010101010101000;
 				B = 16'b1101010101010111;
 			end		
 			
-			1: begin	
+			57: begin	
 				$fdisplay(file,"OP = 11110 	C = !A|!B 								Z, S");
 				OP = 11110; EO = 0; EC = 0; ES = 0; EZ = 1;
 				A = 16'b1111111111111111;
@@ -377,7 +390,7 @@ module TesteFlags();
 			end	
 			
 			// --------------------------------------------------------------------------	
-			1: begin	
+			58: begin	
 				$fdisplay(file,"OP = 11111 	C = 1                                   nenhuma");
 				OP = 11111; EO = 0; EC = 0; ES = 0; EZ = 0;
 			end
